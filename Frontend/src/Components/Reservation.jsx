ef1a73a95f941adc4b1dceede995153fb092c754
Suraj Prasad <surajprasad5656@gmail.com>
2025-03-16 03:21:12 -0700

    commit message here

--- Frontend/src/Components/Reservation.jsx
+++ Frontend/src/Components/Reservation.jsx
@@ -18,7 +18,7 @@ const Reservation = () => {
     e.preventDefault()
     try {
       const { data } = await axios.post(
-        "http://localhost:1000/api/v1/reservation/send",
+        "https://foodheaven-1.onrender.com/api/v1/reservation/send",
         {
           firstname,
           lastname,
