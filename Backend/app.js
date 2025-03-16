407bc1475656bee1140696a12e912b1406dc15d6
Suraj Prasad <surajprasad5656@gmail.com>
2025-03-16 04:28:41 -0700

    first commit

--- Backend/app.js
+++ Backend/app.js
@@ -3,23 +3,26 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { dbconnection } from "./Database/db.js";
 import { errorMiddleware } from "./Error/error.js";
-import reservationrouter from "./Routes/reservationroute.js"
+import reservationrouter from "./Routes/reservationroute.js";
 
 const app = express();
 dotenv.config({ path: "./Config/config.env" });
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
-    methods: ["POST"],
-    credentials: true,
+    origin: (origin, callback) => {
+      callback(null, true); // Allow all origins dynamically
+    },
+    credentials: true, // Allow cookies and tokens
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use("/api/v1/reservation", reservationrouter);
 
 dbconnection();
-app.use(errorMiddleware)
+app.use(errorMiddleware);
 export default app;
